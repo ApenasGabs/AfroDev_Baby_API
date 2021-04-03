63013f0cc374b1d46f938d90e94db363265ae98d
ApenasGabs <gabers357@gmail.com>
2021-04-03 09:59:43 -0300

    branch nlsc is alive

--- config/configExpress.js
+++ config/configExpress.js
@@ -2,11 +2,15 @@ const express = require('express');
 const consign = require('consign');
 
 module.exports = () => {
+
     const app = express()
+
     app.use(express.json());
+
     consign()
         .include('controllers')
         .into(app)
-    return app;
+
+    return app
 
 }
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,19 @@
+const Agendamento = require('../models/Agendamento');
+
 module.exports = app => {
     app.get('/agendamentos', (req, resp) => {
-        resp.send('Servidor ta monstro 😎');
+        Agendamento.listagem(resp);
     });
+
+    app.post('/agendamentos', (req, resp) => {
+        const agendamento = req.body;
+        Agendamento.inserir(agendamento, resp);
+    });
+
+    app.get('/agendamentos/:id', (req, resp) => {
+        const id = parseInt(req.params.id)
+        Agendamento.buscaPorId(id, resp)
+    })
+
 };
+    
